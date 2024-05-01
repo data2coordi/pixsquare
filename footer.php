4ea407a007cac1d660ce706fa322d70d14a7eaa1
data2coordi <data2coordi@gmail.com>
2024-05-01 23:38:52 +0000

    theme check on the way and fixed information

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,46 +12,36 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
+<footer id="colophon" class="site-footer">
 
-		<div class="site-info">
+	<div class="site-info">
 
-			
-			<?php echo "<br>" . get_option('copy_right'); ?>
-			<br>
-			<span class="sep">  </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'pixsquare' ), 'Create', '<a class="create_footer" href="https://color.toshidayurika.com/">Yurika Toshida at Aurora Lab</a>' );
-				?>
-			<a  class="create_footer" href="<?php echo esc_url( __( 'https://wordpress.org/', 'pixsquare' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'pixsquare' ), 'WordPress' );
-				?>
-			</a>
-			<script>
-			$(function () {
-  $("#js-pagetop").click(function () {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 300);
-  });
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 1) {
-      $('#js-pagetop').fadeIn(300).css('display', 'flex')
-    } else {
-      $('#js-pagetop').fadeOut(300)
-    }
-  });
-});
- </script>
 
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		<?php echo "<br>" . get_option('copy_right'); ?>
+		<br>
+		<span class="sep"> </span>
+		<?php
+		/* translators: 1: Theme name, 2: Theme author. */
+		printf(esc_html__('Theme: %1$s by %2$s.', 'pixsquare'), 'Create', '<a class="create_footer" href="https://color.toshidayurika.com/">Yurika Toshida at Aurora Lab</a>');
+		?>
+		<a class="create_footer" href="<?php echo esc_url(__('https://wordpress.org/', 'pixsquare')); ?>">
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf(esc_html__('Proudly powered by %s', 'pixsquare'), 'WordPress');
+			?>
+		</a>
+
+
+
+
+
+
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -13,8 +14,9 @@
 
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">-->
 	<link rel="profile" href="https://gmpg.org/xfn/11">
@@ -23,12 +25,12 @@
 
 	<!-- ハンバーガーメニュー _s -->
 	<div class="drawer">
-	    <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
-	    <input type="checkbox" id="drawer-check" class="drawer-hidden" >
+		<!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+		<input type="checkbox" id="drawer-check" class="drawer-hidden">
 
-	    <!-- ハンバーガーアイコン -->
-	    <label for="drawer-check" class="drawer-open"><span></span></label>
-	 </div>
+		<!-- ハンバーガーアイコン -->
+		<label for="drawer-check" class="drawer-open"><span></span></label>
+	</div>
 	<!-- ハンバーガーメニュー _e -->
 
 
@@ -36,59 +38,48 @@
 </head>
 
 <body <?php body_class('createpc createp1 createp2 createp3 createp4 createp5'); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pixsquare' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'pixsquare'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
+				<?php
 
-			if (has_custom_logo()) :	
-				the_custom_logo();
-			else :
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
+				if (has_custom_logo()) :
+					the_custom_logo();
 				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
-			endif;
-
-
-			$pixsquare_description = get_bloginfo( 'description', 'display' );
-			if ( $pixsquare_description || is_customize_preview() ) :
+					if (is_front_page() && is_home()) :
 				?>
-				<p class="site-description"><?php echo $pixsquare_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+						<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+					<?php
+					else :
+					?>
+						<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+					<?php
+					endif;
+				endif;
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pixsquare' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id' => 'gNavi',
-					'container_class' => 'clearfix',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
 
-		<!-- ハンバーガーメニューjs _s -->
-		<script>
-			// スマートフォン用のハンバーガーアイコン制御
-			const drawerInput = document.getElementById('drawer-check');
-			const navList = document.querySelector('.menu');
-			const drawerOpen = document.querySelector('.drawer-open');
+				$pixsquare_description = get_bloginfo('description', 'display');
+				if ($pixsquare_description || is_customize_preview()) :
+					?>
+					<p class="site-description"><?php echo $pixsquare_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+												?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 
-			drawerOpen.addEventListener('click', () => {
-			navList.classList.toggle('active');
-			});
-		</script>
-		<!-- ハンバーガーメニューjs _e -->
-	</header><!-- #masthead -->
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'pixsquare'); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id' => 'gNavi',
+						'container_class' => 'clearfix',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+
+		</header><!-- #masthead -->
